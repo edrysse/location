1a2fc86218c4602ee6ee431a893fa1e489237816
edrysse <117366780+edrysse@users.noreply.github.com>
2025-03-24 11:41:28 +0000

    hh

--- database/migrations/2025_03_10_035704_create_reviews_table.php
+++ database/migrations/2025_03_10_035704_create_reviews_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,15 +8,19 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('position');
-            $table->text('comment');
-            $table->integer('rating')->default(5);
-            $table->string('avatar')->nullable();
-            $table->timestamps();
-        });
+        // تحقق إذا كان الجدول موجودًا
+        if (!Schema::hasTable('reviews')) {
+            // إنشاء الجدول إذا لم يكن موجودًا
+            Schema::create('reviews', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('position');
+                $table->text('comment');
+                $table->integer('rating')->default(5);
+                $table->string('avatar')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
