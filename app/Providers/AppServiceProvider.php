0c7fc436e58b41d1a038bf06b7f024a05761fbdd
edrysse <117366780+edrysse@users.noreply.github.com>
2025-04-24 17:15:06 +0100

    problem sloved

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,11 +18,6 @@ class AppServiceProvider extends ServiceProvider
 public function boot(): void
 {
 
-    if (env('APP_ENV') === 'production') {
-        URL::forceScheme('https'); // ✅ فرض HTTPS في بيئة الإنتاج
-    }
-
-
     Schema::defaultStringLength(191); // ✅ تجنب مشاكل قواعد البيانات
 
     // ✅ ضبط اللغة في الجلسة إذا لم يتم تعيينها مسبقًا
